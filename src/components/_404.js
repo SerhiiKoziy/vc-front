c3d2c22ddeaf4969e540ebfac967a9cf4b5844fd
Serhii <s.koziy@mobilunity.com>
2017-05-24 16:33:12 +0300

    del other

--- src/components/_404.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const _404 = () => {
-  return <div>Error 404</div>;
-};
-
-export default _404;
--- src/redux/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import { routerReducer } from 'react-router-redux';
-import applicationReducer from './application/reducer';
-
-export default combineReducers({
-  routing: routerReducer,
-  application: applicationReducer,
-});
